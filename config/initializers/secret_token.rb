844eca102e2aef27db6ee086eb89607f83fb10a4
yleo77 <ylep77@gmail.com>
2013-06-30 19:36:44 +0800

    xiaopihai's birthday

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xiaopihai::Application.config.secret_token = 'b27b367ae57c3f91e6d79700061ad8e0732a2ab2099773f2fa16907cd40b0b6c7ca57195c3f135b6c16f7753a5ed550828fb0a97fe04047f026668c7ac35cd9e'
